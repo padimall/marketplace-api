82d36c6cbc9fdc37da3388bdfa6fc3843944a307
rudi97277 <rudi97277@gmail.com>
2020-08-31 09:00:01 +0700

    edit htaccess

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Builder::defaultStringLength(191);
+        // Builder::defaultStringLength(191);
     }
 }
