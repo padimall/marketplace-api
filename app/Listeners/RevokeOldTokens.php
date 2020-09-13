da1fbb75a5c97c34f9a487882042bea7d85a59f6
padimall <padimall.bisnis@gmail.com>
2020-09-13 21:14:45 +0700

    edit listener

---
+++ app/Listeners/RevokeOldTokens.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Listeners;
+
+//use App\Events\Laravel\Passport\Events\AccessTokenCreated;
+
+use Laravel\Passport\Events\AccessTokenCreated;
+//use Laravel\Passport\Events\RefreshTokenCreated;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class RevokeOldTokens
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  AccessTokenCreated  $event
+     * @return void
+     */
+    public function handle(AccessTokenCreated $event)
+    {
+        //
+    }
+}
