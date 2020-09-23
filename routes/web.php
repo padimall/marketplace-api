0fd7e785fb6b45c7bc5d1c9cfbfae3f0fd86bab7
rudi97277 <rudi97277@gmail.com>
2020-09-23 17:22:08 +0700

    404

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     // return view('welcome');
-    return view('notfound');
+    // return view('notfound');
+    return Response::view('errors',array(),404);
 });
