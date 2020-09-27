3be089659f2c03bcd94c84f0acefc7edea120626
padimall <padimall.bisnis@gmail.com>
2020-09-27 15:52:25 +0700

    from server

--- app/Product.php
+++ app/Product.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use \App\Http\Traits\UsesUuid;
-    protected $fillable = ['supplier_id','name','price','weight','description','category','stock','status'];
+    protected $fillable = ['supplier_id','name','price','weight','description','category','stock','min_order','status'];
 }
