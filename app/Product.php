040931243a1c0c21b8488ecd834d2f36dada4856
rudi97277 <rudi97277@gmail.com>
2020-11-05 20:32:30 +0700

    enable soft delete

--- app/Product.php
+++ app/Product.php
@@ -9,5 +9,6 @@ class Product extends Model
 {
     use SoftDeletes;
     use \App\Http\Traits\UsesUuid;
+    protected $softDelete = true;
     protected $fillable = ['supplier_id','name','price','weight','description','category','stock','min_order','status','agent_id','admin_price'];
 }
