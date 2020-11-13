37af10f75a94c1baf5db4e02e72d31e1038017b4
rudi97277 <rudi97277@gmail.com>
2020-11-13 17:13:44 +0700

    post callback

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 */
 Route::post('test','InvoiceController@testXendit');
 Route::get('test2','InvoiceController@createInvoice');
-Route::get('callback','InvoiceController@callback');
+Route::post('callback','InvoiceController@callback');
 Route::get('email/verify/', 'VerificationController@verify')->name('verification.verify'); // Make sure to keep this as your route name
 
 Route::get('email/resend', 'VerificationController@resend')->name('verification.resend');
