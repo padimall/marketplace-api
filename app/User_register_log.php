5697a1603c25b2faa1b7f3335f0dde20fe34bc4e
rudi97277 <rudi97277@gmail.com>
2020-11-16 08:55:14 +0700

    edit model

--- app/User_register_log.php
+++ app/User_register_log.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class User_register_log extends Model
 {
-    protected $fillable = ['user','location'];
+    protected $fillable = ['user','city','country'];
 }
