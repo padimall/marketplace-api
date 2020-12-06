56fb6f87a3983c8f93499612927748d88bc6b5d4
rudi97277 <rudi97277@gmail.com>
2020-12-06 17:51:51 +0700

    fix

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -656,55 +656,49 @@ class InvoiceController extends Controller
             ],200);
         }
 
-        $testing = array();
-        array_push($testing,sizeof($data));
+        for($in=0; $in<sizeof($data); $in++)
+        {
+            $logistic = DB::table('invoices_logistics')
+                        ->join('logistics','logistics.id','=','invoices_logistics.logistic_id')
+                        ->where('invoices_logistics.invoice_id',$data[$in]->id)
+                        ->select('logistics.name')
+                        ->first();
 
-        // for($i=0; $i<sizeof($data); $i++)
-        // {
-        //     $logistic = DB::table('invoices_logistics')
-        //                 ->join('logistics','logistics.id','=','invoices_logistics.logistic_id')
-        //                 ->where('invoices_logistics.invoice_id',$data[$i]->id)
-        //                 ->select('logistics.name')
-        //                 ->first();
+            $product = DB::table('invoices_products')
+                        ->where('invoice_id',$data[$in]->id)
+                        ->get();
 
-        //     $product = DB::table('invoices_products')
-        //                 ->where('invoice_id',$data[$i]->id)
-        //                 ->get();
+            $listProduct = array();
+            for($i=0; $i<sizeof($product); $i++)
+            {
+                array_push($listProduct,$product[$i]->product_id);
+            }
 
+            $image = DB::table('products_images')
+                        ->whereIn('product_id',$listProduct)
+                        ->get();
 
+            for($i=0; $i<sizeOf($product); $i++)
+            {
+                for($j=0; $j<sizeOf($image); $j++)
+                {
+                    if($image[$j]->product_id==$product[$i]->product_id){
+                        $product[$i]->image = url('/').'/'.$image[$j]->image;
+                        break;
+                    }
+                }
+            }
 
-        //     $listProduct = array();
-        //     for($i=0; $i<sizeof($product); $i++)
-        //     {
-        //         array_push($listProduct,$product[$i]->product_id);
-        //     }
+            $data[$in]->logistic = $logistic;
+            $data[$in]->products = $product;
+        }
 
-        //     $image = DB::table('products_images')
-        //                 ->whereIn('product_id',$listProduct)
-        //                 ->get();
-
-        //     for($i=0; $i<sizeOf($product); $i++)
-        //     {
-        //         for($j=0; $j<sizeOf($image); $j++)
-        //         {
-        //             if($image[$j]->product_id==$product[$i]->product_id){
-        //                 $product[$i]->image = url('/').'/'.$image[$j]->image;
-        //                 break;
-        //             }
-        //         }
-        //     }
-
-        //     $data[$i]->logistic = $logistic;
-        //     $data[$i]->products = $product;
-        // }
-
-        // $group->invoices = $data;
+        $group->invoices = $data;
 
         return response()->json([
             'status' => 1,
             'message' => 'Resource found!',
-            'data' => $testing,
-            'group' => $group,
+            'data' => $group
         ],200);
 
     }
