7355f301d34be1ed7267e28be8ba18031e645624
rudi97277 <rudi97277@gmail.com>
2020-12-09 21:41:18 +0700

    show all

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -861,7 +861,7 @@ class InvoiceController extends Controller
                 return response()->json([
                     'status' => 1,
                     'message' => 'Resource found',
-                    'data' => $show
+                    'data' => $getInvoice
                 ],200);
             }
             else {
