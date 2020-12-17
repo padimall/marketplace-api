42c6e1bf2541f08ce794584face3e86e02c963c3
rudi97277 <rudi97277@gmail.com>
2020-12-17 23:41:14 +0700

    status = 0

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -602,6 +602,7 @@ class ProductController extends Controller
         $data = DB::table('products')
                 ->where('agent_id',$agent_data->id)
                 ->whereNull('deleted_at')
+                ->where('status',0)
                 ->select('*')
                 ->get();
         // Product::where('agent_id',$agent_data->id)->get();
