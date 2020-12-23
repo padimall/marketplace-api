50f80ba4e4f2b5093c8bdd79d4fb71a126501df1
rudi97277 <rudi97277@gmail.com>
2020-12-23 17:45:02 +0700

    fix send

--- app/helper/Helper.php
+++ app/helper/Helper.php
@@ -14,14 +14,9 @@ class Helper
             ]
         ]);
 
-        $message = [
-            'to'=>$target,
-            'data'=>$data
-        ];
-        $response = $client->request('POST','https://fcm.googleapis.com/fcm/send?',
-        [
-            ['body' => json_encode($message)]
-        ]);
+        $input = 'to='.$target.'&data='.json_encode($data);
+
+        $response = $client->request('POST','https://fcm.googleapis.com/fcm/send?'.$input);
         $response = json_decode($response->getBody(),TRUE);
         return $response;
     }
