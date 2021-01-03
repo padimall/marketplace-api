cea8957e3eb50fe746336e4fe1b91b263cce6edc
rudi97277 <rudi97277@gmail.com>
2021-01-03 12:55:41 +0700

    w

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -188,9 +188,10 @@ class ProductController extends Controller
             $data->image = NULL;
         }
 
+        $product_id = $request['target_id'];
         $ratings = DB::table('invoices_product_ratings')
                     ->join('invoices_products','invoices_products.id','=','invoices_product_ratings.invoice_product_id')
-                    ->join(DB::raw('(SELECT COUNT(IPR.id) AS total_ratings,SUM(IPR.star) AS total_star FROM invoices_product_ratings AS IPR INNER JOIN invoices_products AS IP ON IPR.invoice_product_id = IP.id WHERE IP.id = $request["target_id"]) rating_sumarry'),DB::raw('1'),'=',DB::raw('1'))
+                    ->join(DB::raw('(SELECT COUNT(IPR.id) AS total_ratings,SUM(IPR.star) AS total_star FROM invoices_product_ratings AS IPR INNER JOIN invoices_products AS IP ON IPR.invoice_product_id = IP.id WHERE IP.id = $product_id) rating_sumarry'),DB::raw('1'),'=',DB::raw('1'))
                     ->where('invoices_products.product_id',$request['target_id'])
                     ->select('invoices_product_ratings.*','rating_sumarry.*')
                     ->first();
